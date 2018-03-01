c2d8fe25b656a5781cc7b2b79ace47eee3eb32cc
chongqingwuyifan <chongqingwuyifan@10.10.10.102>
2018-03-02 00:25:41 +0800

    update

---
+++ src/com/zc/cris/n21/both/Order.java
@@ -0,0 +1,30 @@
+package com.zc.cris.n21.both;
+
+
+public class Order {
+	
+	private Integer id;
+	private String name;
+	private Customer customer;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+}
